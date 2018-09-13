d9d6ab8c2ec57cc351f1eb91517c63e24ad4d115
cassadys <skylercassady@gmail.com>
2018-09-12 22:26:44 -0600

    Adding course-work p5 example and committing hw-3

---
+++ hw-3/sketch.js
@@ -0,0 +1,16 @@
+//create setup function
+function setup() {
+  //create a canvas 800x wide, 600px high
+  createCanvas( 800, 600 );
+
+  //make background purple
+  background( 'purple' );
+}
+
+
+//declae draw() function
+function draw() {
+
+  //use text function to draw "Hello World!"
+  text( "Hello World!", 370, 290 )
+}
