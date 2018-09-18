b89c25488bd4806151fde11730d004745528a469
cassadys <skylercassady@gmail.com>
2018-09-18 12:38:18 -0600

    created white ellipse for eye

--- hw-4/sketch.js
+++ hw-4/sketch.js
@@ -15,9 +15,13 @@ rect(300, 240, 200, 120, 40);
 //create triangles for horn rgb fill horn 252 251 228
 fill( 252, 251, 228 );
 triangle(350, 150, 450, 150, 400, 50)
+//draw lines for horn spiraling
 line(350, 150, 450, 140)
 line(350, 130, 450, 120)
 line(350, 110, 450, 100)
 line(350, 90, 450, 80)
 line(350, 70, 450, 60)
+//draw ellipse for eye fill white
+fill( "white" )
+ellipse(400, 195, 80)
 }
