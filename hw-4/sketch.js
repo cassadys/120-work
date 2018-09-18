cbc1c93aa4dde5192f5ed0097f9c4079bc992363
cassadys <skylercassady@gmail.com>
2018-09-18 12:23:48 -0600

    finished horn spiral

--- hw-4/sketch.js
+++ hw-4/sketch.js
@@ -11,8 +11,13 @@ function draw(){
 fill( "purple" );
 rect(200, 150, 400, 300, 100);
 //create rectangle for nose
-rect(300, 240, 200, 120, 40)
+rect(300, 240, 200, 120, 40);
 //create triangles for horn rgb fill horn 252 251 228
-fill( 252, 251, 228 )
+fill( 252, 251, 228 );
 triangle(350, 150, 450, 150, 400, 50)
+line(350, 150, 450, 140)
+line(350, 130, 450, 120)
+line(350, 110, 450, 100)
+line(350, 90, 450, 80)
+line(350, 70, 450, 60)
 }
