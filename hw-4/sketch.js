12ef349369256f60729009f4c6d5f0bc551df947
cassadys <skylercassady@gmail.com>
2018-09-18 17:10:41 -0600

    update color

--- hw-4/sketch.js
+++ hw-4/sketch.js
@@ -8,9 +8,10 @@ function draw(){
 //create rectangle close to center of canvas 400 pixels wide and 300 high for head
 //rounded edges 100 pixels
 //fill face "purple"
-fill( "purple" );
+fill(75, 62, 154);
 rect(200, 150, 400, 300, 100);
 //create rectangle for nose
+fill(58, 48, 145);
 rect(300, 240, 200, 120, 40);
 //create triangles for horn rgb fill horn 252 251 228
 fill( 252, 251, 228 );
