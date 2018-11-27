ee1d9d4562ed01c4751c970b11ba496692e57549
cassadys <skylercassady@gmail.com>
2018-11-27 14:31:38 -0500

    homework 8 and readme

--- hw-8/readme.md
+++ hw-8/readme.md
@@ -1,5 +1,20 @@
-# Empty p5 sketch.
+Skyler Cassady, 50
 
-Please replace contents of this file with appropriate readme information after finishing your p5 sketch.
+# HW 8 For and While Loops #
 
-Current p5 version -> 0.7.1
+This week's homework is about for and while loops.  For loops can be used to repeat a command or set of commands as long as a condition is met.  For my assignment this week I used a nested for loop to make a grid of tiny circles covering the screen.
+
+## Grid of Color Changing Ellipses ##
+
+1. createCanvas width by height
+2. set noStroke()
+3. Define variables
+4. set variable d to width/80 to make 80 circles
+5. change 80 to variable "eighty" so number of circles can be easily changed
+6. use nested for loop to make eighty circles across x axis and then add "d" to the y axis d=diameter of circle
+7. ellipse color code: fill((255/eighty*i), color2, random(255));
+8. ellipse draw code: ellipse(pos_x+r, pos_y+r, d);
+9. ellipse x axis code: pos_x = width/eighty*i;
+10. add mousePressed function to add +10 to color2 every click and reset at 255 to 0
+11. ellipse to show color2 value as it is changed by clicking
+    code: ellipse(((width/255)*color2), 30, 30)
--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -6,87 +6,54 @@ function setup() {
 }
 
 //define variables
+let color1 = 255
+let color2 = 0
 pos_x = 0
 pos_y = 0
 let d = 10
 let r = 5
 let i = 0
 let k = 0
+let eighty = 80
 
 //run draw function; set d for ellipse diameter to make 80 ellipses with for loop
 function draw() {
-  let d = width/81
-	background("green");
-  //1st row of ellipses
-	for (let i = 0; i < 80; i++) {
+  let d = width/eighty
+  let r = d/2
+	background( abs(color1) );
+  for (let pos_y = d; pos_y < height; pos_y += d) {
+  //1st row of ellipses using for fuction to draw ellipses every
+	for (let i = 0; i < eighty; i++) {
     //fill to scale white to grey over ellipses
-    fill((255/80*i));
+    fill((255/eighty*i), color2, random(255));
+    //make ellipse pos_x+radius of circle pos_y+radius of circle to keep circle on screen
 		ellipse(pos_x+r, pos_y+r, d);
     //shift ellipses on x axis with k variable increasing every frame
-    pos_x = width/80*i+k;
+    //pos_x is width divided by 80xi to get 80 evenly spaced circles along x axis
+    pos_x = width/eighty*i;
+    ellipse(((width/255)*color2), 30, 30)
   }
-//2nd row of 80 ellipses
-  for (let i = 0; i < 80; i++) {
-    fill((255/80*i));
-    ellipse(pos_x+r, pos_y+r+1/10*height, d);
-    pos_x = width/80*i+k;
-  }
-//3rd row of ellipses
-  for (let i = 0; i < 80; i++) {
-    fill((255/80*i));
-    ellipse(pos_x+r, pos_y+r+2/10*height, d);
-    pos_x = width/80*i+k;
-  }
-//4th row of ellipses
-  for (let i = 0; i < 80; i++) {
-    fill((255/80*i));
-    ellipse(pos_x+r, pos_y+r+3/10*height, d);
-    pos_x = width/80*i+k;
-  }
-//5th row of ellipses
-  for (let i = 0; i < 80; i++) {
-    fill((255/80*i));
-    ellipse(pos_x+r, pos_y+r+4/10*height, d);
-    pos_x = width/80*i+k;
-  }
-//6th row of ellipses
-  for (let i = 0; i < 80; i++) {
-    fill((255/80*i));
-    ellipse(pos_x+r, pos_y+r+5/10*height, d);
-    pos_x = width/80*i+k;
-  }
-//7th row of ellipses
-  for (let i = 0; i < 80; i++) {
-    fill((255/80*i));
-    ellipse(pos_x+r, pos_y+r+6/10*height, d);
-    pos_x = width/80*i+k;
-  }
-//8th row of ellipses
-  for (let i = 0; i < 80; i++) {
-    fill((255/80*i));
-    ellipse(pos_x+r, pos_y+r+7/10*height, d);
-    pos_x = width/80*i+k;
-  }
-//9th row of ellipses
-  for (let i = 0; i < 80; i++) {
-    fill((255/80*i));
-    ellipse(pos_x+r, pos_y+r+8/10*height, d);
-    pos_x = width/80*i+k;
-  }
-//10th row of ellipses
-  for (let i = 0; i < 80; i++) {
-    fill((255/80*i));
-    ellipse(pos_x+r, pos_y+r+9/10*height, d);
-    pos_x = width/80*i+k;
-  }
+}
+}
 //increases pos_y +1 every frame and reset to 0 when it is greater than windowHeight
-    pos_y++;
-    if (pos_y > height) {
-      pos_y = 0;
-	}
-  //increases k variable by +1 every frame up to 15 and resets; added to pos_x to make ellipses move 15 pixels to the right and reset to 0 
-  k++;
-  if (k > 15) {
-    k = 0
+  //  pos_y++;)
+    //if (pos_y > height) {
+  //    pos_y = 0;
+//	}
+  //increases k variable by +1 every frame up to 15 and resets; added to pos_x to make ellipses move 15 pixels to the right and reset to 0
+  //k++;
+//  if (k > 15) {
+//    k = 0
+//  }
+//subtract 1 from color1 and reset to 255 at -255 abs(color1) used for background
+color1--;
+if (color1 < -254) {
+  color1 = 255
+}
+
+function mousePressed(){
+  color2 += 10
+  if (color2 > 255) {
+    color2 = 0
   }
 }
